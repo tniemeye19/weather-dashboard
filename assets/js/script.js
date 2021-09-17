f55beade36eb7186a0e7e3598ac8ca2cf91c540c
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-17 15:44:32 -0400

    getWeather function created to get weather object from lat and long from city input

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+function getWeather(city) {
+
+    var apiKey = '91d4161a1433d45e7d7152f7adf492fd'
+
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+
+    fetch(requestUrl)
+        .then(function(response) {
+           return response.json()
+        })
+        .then(function(data){
+            var lon = data[0].lon
+            var lat= data[0].lat
+
+            var cityWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        
+            fetch(cityWeatherUrl)
+                .then(function(response) {
+                    return response.json()
+                })
+                .then(function(data) {
+                 
+                })
+        })
+
+}
+
+
