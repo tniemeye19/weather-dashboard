08e594926e395afa7a8dd0e47cdb75db26256dc3
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-17 18:40:23 -0400

    added html elements to js, api data elements now visible on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 var searchBtnEl = document.querySelector("#city-search");
 var cityEl = document.querySelector("#city");
+var searchedCityName = document.querySelector("#searched-city-name");
+var currentDateEl = document.querySelector("#currentDate");
+var weatherIconEl = document.querySelector("#weather-icon");
+var currentTempEl = document.querySelector("#temperature");
+var currentHumidityEl = document.querySelector("#humidity");
+var currentWindSpeedEl = document.querySelector("#wind-speed");
+var currentUVIndexEl = document.querySelector("#uv-index");
 
 function getWeather(city) {
 
@@ -13,18 +20,23 @@ function getWeather(city) {
         })
         .then(function(data){
             console.log("location", data)
-        //     var lon = data[0].lon
-        //     var lat= data[0].lat
+            var lon = data[0].lon
+            var lat= data[0].lat
 
-        //     var cityWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+            var cityWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
         
-        //     fetch(cityWeatherUrl)
-        //         .then(function(response) {
-        //             return response.json()
-        //         })
-        //         .then(function(data) {
-        //             console.log(data);
-        //         })
+            fetch(cityWeatherUrl)
+                .then(function(response) {
+                    return response.json()
+                })
+                .then(function(data) {
+                    console.log("weather", data);
+
+                    currentTempEl.innerHTML = "Temperature: " + data.current.temp + "°F";
+                    currentHumidityEl.innerHTML = "Humidity: " + data.current.humidity + "%";
+                    currentWindSpeedEl.innerHTML = "Wind Speed: " + data.current.wind_speed + "MPH";
+                    currentUVIndexEl.innerHTML = "UV-Index: " + data.current.uvi; 
+                })
         })
 
 }
@@ -33,5 +45,6 @@ searchBtnEl.addEventListener("click", function(event) {
     event.preventDefault();
     var searchedCity = cityEl.value;
     getWeather(searchedCity);
+    searchedCityName.innerHTML = searchedCity;
 
 });
